0fa51f47dfcc7829e9535b1708cc2cd98b5e6050
Varun Barad <varun@varunbarad.com>
2019-10-19 12:04:18 +0530

    Initial commit.

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const isValidDate = (date) => {
+    return ((date instanceof Date) && !isNaN(date));
+};
+
+const requestHandler = (request, response) => {
+    const date = new Date(request.params.dateString);
+    if (isValidDate(date)) {
+        response.json({
+            unix: date.getTime(),
+            utc: date.toUTCString()
+        });
+    } else {
+        response.json({
+            error: 'Invalid Date'
+        });
+    }
+};
+
+app.get('/api/timestamp/', function (request, response, next) {
+    request.params.dateString = (new Date()).toISOString();
+    next();
+}, requestHandler);
+
+app.get('/api/timestamp/:dateString', requestHandler);
+
+app.listen(8000);
